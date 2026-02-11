b582d7513dcc5f0065443cfe006d34286db1c044
B1EL124 <161520000+B1EL124@users.noreply.github.com>
2026-02-11 19:23:15 +0000

    esse tal de vue seloko

---
+++ vue-project/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+function incrementar() {
+  count.value++
+}
+</script>
+
+<template>
+  <div style="padding: 40px">
+    <h1>Projeto Vue 3 - Aula Prática</h1>
+    
+    <p>Contador: {{ count }}</p>
+    
+    <button @click="incrementar">
+      Incrementar
+    </button>
+  </div>
+</template>
